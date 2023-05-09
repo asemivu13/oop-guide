668581e48bbfa874f1abec80867ce142e9f77017
asemivu13 <asemivu13@gmail.com>
2023-05-09 11:50:39 +0300

    Base commit

---
+++ src/InheritanceVsComposition/Image.java
@@ -0,0 +1,37 @@
+package InheritanceVsComposition;
+
+public abstract class Image {
+    private Pixel pixel;
+    private int width;
+    private int height;
+
+    public Image() {
+        this(0, 0);
+    }
+    public Image(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+    public void setSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.pixel = new Pixel(width, height);
+    }
+    public Pixel pixelAt(int x, int y) {
+        return new Pixel(x, y);
+    }
+    public void replacePixels(Pixel pixel) {
+        System.out.println("replace pixel");
+    }
+    public void replace(double scale) {
+        System.out.println("resize");
+    }
+    public void flipHorizontal() {
+        System.out.println("flip horizontal");
+    }
+    public void flipVertical() {
+        System.out.println("flip vertical");
+    }
+    public abstract void load();
+    public abstract void save();
+}
---
+++ src/InheritanceVsComposition/Pixel.java
@@ -0,0 +1,11 @@
+package InheritanceVsComposition;
+
+public class Pixel {
+    private int width;
+    private int height;
+
+    public Pixel(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
