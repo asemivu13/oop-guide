5aa927b5d455422dabe53746747dde08ca1ee0e5
asemivu13 <asemivu13@gmail.com>
2023-05-09 13:30:33 +0300

    Encapsulation

---
+++ src/encapsulation/Animal.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class Animal {
+    private String name;
+    private int age;
+
+    public void makeSound() {
+        System.out.println("Animal: makeSound");
+    }
+    public void walk() {
+        System.out.println("Animal: walk");
+    }
+}
---
+++ src/encapsulation/ReadME.md
@@ -0,0 +1,5 @@
+# Encapsulation
+internal state: encapsulated which mean
+1. method is the only way to change an object's internal data
+2. it can't be accessed directly
+3. its state invisible from outside the object
